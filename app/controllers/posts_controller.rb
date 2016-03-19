20fc22e180ecfe089621fb2869a6d63846352565
ibadboy <polawat.job@gmail.com>
2016-03-19 04:28:58 +0000

    add style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,17 +24,14 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
+      @post = Post.new(post_params)
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        #posts_url(@post.id) >> @post
+        redirect_to @post, notice: 'Post was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
+  
   end
 
   # PATCH/PUT /posts/1
